a2d960f0f89b668341012d203223362dcfc5cc1e
Bhupendra simha <bhupendrasimha@gmail.com>
2023-05-29 17:39:22 +0530

    feat/chat gpt integration

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -7,10 +7,7 @@ const Popup = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
+        <p></p>
         <a
           className="App-link"
           href="https://reactjs.org"
